ad97b556fb33f58f6af6dc16df918adcd31502b9
Pietrell <piotrplotast2006@gmail.com>
2023-08-26 16:37:46 +0200

    added js functionality - form submitting

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const submitBtn = document.querySelector('.submit-btn');
 const ratingCard = document.querySelector('.rating-card');
 const thankYouCard = document.querySelector('.thankyou-card');
+const form = document.querySelector('form');
+const userRatingSpan = document.querySelector('.user-rating');
+
 function submitForm() {
     ratingCard.style.display = 'none';
-    thankYouCard.style.display = 'block'
+    thankYouCard.style.display = 'block';
+    const selectedOption = document.querySelector('input[name="rating"]:checked');
+    const selectedValue = selectedOption.value;
+    userRatingSpan.innerHTML = selectedValue;
+
 };
 
 
-
-
-submitBtn.addEventListener('click', submitForm);
+form.addEventListener('submit', submitForm);
